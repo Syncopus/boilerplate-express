47c6bac42e6aaf232e3648098d05e995699ef23e
Syncopus <jules.chevallet@gmail.com>
2023-06-29 10:21:58 +0200

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let app = express();
+function handleRequest(req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+}
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/', handleRequest);
+
 
 
 
